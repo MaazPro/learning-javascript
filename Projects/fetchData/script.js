29199369c0a01543166e9c109cc059b9f33bcbc3
Maaz <maaz@NIS-PK412s-MacBook-Pro.local>
2024-09-16 12:11:57 +0500

    Project completed

---
+++ Projects/fetchData/script.js
@@ -0,0 +1,25 @@
+const apiURL = 'https://jsonplaceholder.typicode.com/users'
+let data = '';
+const div1 = document.querySelector(".div1")
+function fetchUserData(){
+    return new Promise((resolve, reject)=>{
+        fetch(apiURL)
+        .then(response =>{
+            if (!response.ok)
+            {
+                reject("Error: Unable to fetch data!")
+            }
+            return response.json()
+        })
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+    })
+}
+
+fetchUserData()
+.then(users =>{
+    console.log(users);
+})
+.catch(error =>{
+    console.log(error);
+})
